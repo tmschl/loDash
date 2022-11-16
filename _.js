3f6d17cb9cf078933e8c997aa6f302dddb110d4a
tmschl <timothy.schiller@gmail.com>
2022-11-16 16:11:39 -0500

    control flow solution

--- _.js
+++ _.js
@@ -1,6 +1,22 @@
+const _ = {
+  clamp (num, lowBound, highBound) {
+    if (num < lowBound) {
+      num = lowBound;
+      console.log(num, 'low');
+      return lowBound;
+    } else if (num > highBound) {
+      num = highBound;
+      console.log(num, 'high')
+      return num;
+    } else {
+      console.log(num, 'within')
+      return num;
+    }
+    console.log('hi');
+  }
+};
 
-
-
+_.clamp(21,0,23);
 
 
 // Do not write or modify code below this line.
