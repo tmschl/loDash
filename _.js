364268f5a6279cedee8f51b8e7cd593e0cf5302a
tmschl <timothy.schiller@gmail.com>
2022-11-16 16:32:25 -0500

    solves using Math.max to find min then passing it and num into Math.min to find max

--- _.js
+++ _.js
@@ -1,23 +1,28 @@
 const _ = {
   clamp (num, lowBound, highBound) {
-    if (num < lowBound) {
-      num = lowBound;
-      console.log(num, 'low');
-      return lowBound;
-    } else if (num > highBound) {
-      num = highBound;
-      console.log(num, 'high')
-      return num;
-    } else {
-      console.log(num, 'within')
-      return num;
-    }
-    console.log('hi');
+    let clampMin = Math.max(num, lowBound);
+    let clampedNum = Math.min(clampMin, highBound);
+    return clampedNum
+    
+    // if (num < lowBound) {
+    //   num = lowBound;
+    //   console.log(num, 'low');
+    //   return lowBound;
+    // } else if (num > highBound) {
+    //   num = highBound;
+    //   console.log(num, 'high')
+    //   return num;
+    // } else {
+    //   console.log(num, 'within')
+    //   return num;
+    // }
+
   }
 };
 
-_.clamp(21,0,23);
+let tim = _.clamp(-10,0,23);
 
+console.log(tim);
 
 // Do not write or modify code below this line.
 module.exports = _;
